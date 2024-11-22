84ae950b72acc00ef53a571efe91a2684f82c7a0
olivanaa <oliv.anaclara@gmail.com>
2024-11-22 03:51:24 -0300

    ajuste tabelas

--- src/main/resources/db/migration/V6__create_table_usuario_recompensa.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE usuario_recompensa (
-    usuario_id NUMBER,
-    recompensa_id NUMBER,
-    status VARCHAR2(50) NOT NULL,
-    data_resgate DATE,
-    PRIMARY KEY (usuario_id, recompensa_id),
-    CONSTRAINT fk_usuario FOREIGN KEY (usuario_id) REFERENCES watt_usuario(id),
-    CONSTRAINT fk_recompensa FOREIGN KEY (recompensa_id) REFERENCES watt_recompensa(id)
-);
