c8254ba25b3e6e71cb091fa671c97238efb9abf5
olivanaa <oliv.anaclara@gmail.com>
2024-11-22 05:14:23 -0300

    configuração rabbitmq

--- src/main/java/br/com/fiap/watteco_gs/config/RabbitMQConfig.java
+++ src/main/java/br/com/fiap/watteco_gs/config/RabbitMQConfig.java
@@ -4,10 +4,12 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableRabbit
 public class RabbitMQConfig {
 
     public static final String QUEUE_NAME = "energyQueue";
